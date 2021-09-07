b09b8884b9be7e8618e73c643f4680d70bc9c749
aniket <aniket.umbc@gmail.com>
2021-09-07 19:09:06 +0530

    button added with test cases

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+it('should button has correct initial color', () => {
   render(<App />);
-  const linkElement = screen.getByText('Learn React');
-  expect(linkElement).toBeInTheDocument();
+  const btnEle = screen.getByRole('button', { name: 'Change to blue' });
+  expect(btnEle).toHaveStyle({ backgroundColor: 'red' });
+});
+
+it('should turns blue when clicked', () => {
+  render(<App />);
+  const btnEle = screen.getByRole('button', { name: 'Change to blue' });
+  fireEvent.click(btnEle);
+  expect(btnEle).toHaveStyle({ backgroundColor: 'blue' });
+  expect(btnEle.textContent).toBe('Change to red');
 });
